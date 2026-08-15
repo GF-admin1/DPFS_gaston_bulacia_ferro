885f33883d31ea19276e508cb00bf2bfca2dcab1
GF-admin1 <gbulaciaferro02@gmail.com>
2026-08-15 14:38:11 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 
 // Rutas
 const productsRoutes = require('./routes/productsRoutes');
+const userRoutes = require('./routes/users');
 
 // Configuración del motor de vistas
 app.set('view engine', 'ejs');
@@ -12,23 +13,16 @@ app.set('views', path.join(__dirname, 'views'));
 // Archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Usar las rutas de productos
+// Usar las rutas
 app.use('/products', productsRoutes);
+app.use('/users', userRoutes);
 
 // Ruta principal (Home)
 app.get('/', (req, res) => {
     res.render('index');
 });
 
-// Otras rutas vistas
-app.get('/login', (req, res) => {
-    res.render('users/login');
-});
-
-app.get('/register', (req, res) => {
-    res.render('users/register');
-});
-
+// Otras rutas vistas generales
 app.get('/productCart', (req, res) => {
     res.render('products/productCart');
 });
