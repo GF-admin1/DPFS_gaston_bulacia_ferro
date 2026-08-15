78ea1f9083518148ab718014409009e75bde4e25
GF-admin1 <gbulaciaferro02@gmail.com>
2026-08-15 14:53:00 -0300

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+// 1. Requerir el controlador y el middleware de multer
 const usersController = require('../controllers/usersController');
+const upload = require('../middlewares/multerMiddleware');
 
-// Ruta al formulario de registro
+// Ruta al formulario de registro (GET)
 router.get('/register', usersController.register);
 
-// Ruta al formulario de login
+// Ruta para procesar el registro con Multer para la foto de perfil (POST)
+router.post('/register', upload.single('avatar'), usersController.processRegister);
+
+// Ruta al formulario de login (GET)
 router.get('/login', usersController.login);
 
 module.exports = router;
