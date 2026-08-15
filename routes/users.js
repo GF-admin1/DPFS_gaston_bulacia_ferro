85f79abfa70f9777306950365e00d57362c1187a
GF-admin1 <gbulaciaferro02@gmail.com>
2026-08-15 14:32:31 -0300

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const router = express.Router();
 
+const usersController = require('../controllers/usersController');
+
+// Ruta al formulario de registro
+router.get('/register', usersController.register);
+
+// Ruta al formulario de login
+router.get('/login', usersController.login);
+
+module.exports = router;
