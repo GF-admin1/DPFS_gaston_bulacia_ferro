292caec89c56213b76b42ffc6aaea3777571e480
GF-admin1 <gbulaciaferro02@gmail.com>
2026-08-15 14:57:52 -0300

    Update usersController.js

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -4,12 +4,15 @@ const usersController = {
     },
     
     processRegister: (req, res) => {
-        // Aquí se procesan los datos del formulario y req.file tiene la info de la foto
         res.send('¡Usuario registrado con éxito y foto guardada!');
     },
 
     login: (req, res) => {
         res.render('users/login');
+    },
+
+    processLogin: (req, res) => {
+        res.send('Procesando login...');
     }
 };
 
