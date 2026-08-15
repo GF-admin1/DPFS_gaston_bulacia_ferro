b278b68f0aaa04acea19f9818558280776ea4425
GF-admin1 <gbulaciaferro02@gmail.com>
2026-08-15 14:59:35 -0300

    Update usersController.js

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,10 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+
+const usersFilePath = path.join(__dirname, '../data/users.json');
+
+const getUsers = () => {
+    return JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+};
+
 const usersController = {
     register: (req, res) => {
         res.render('users/register');
     },
     
     processRegister: (req, res) => {
-        res.send('¡Usuario registrado con éxito y foto guardada!');
+        const users = getUsers();
+        
+        const newUser = {
+            id: users.length > 0 ? users[users.length - 1].id + 1 : 1,
+            ...req.body,
+            avatar: req.file ? req.file.filename : 'default.png'
+        };
+
+        users.push(newUser);
+        fs.writeFileSync(usersFilePath, JSON.stringify(users, null, 2));
+
+        res.redirect('/users/login');
     },
 
     login: (req, res) => {
