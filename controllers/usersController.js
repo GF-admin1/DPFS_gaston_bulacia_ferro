65885b8b5bdcc87bdc4466315cfafc3e3ed0a5c6
GF-admin1 <gbulaciaferro02@gmail.com>
2026-08-16 15:57:40 -0300

    Update usersController.js

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,72 +1,70 @@
-const fs = require('fs');
-const path = require('path');
 const bcrypt = require('bcryptjs');
-
-const usersFilePath = path.join(__dirname, '../data/users.json');
-
-const getUsers = () => {
-    return JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
-};
+const db = require('../database/models');
 
 const usersController = {
     register: (req, res) => {
-        res.render('users/register');
+        return res.render('users/register');
     },
     
-    processRegister: (req, res) => {
-        const users = getUsers();
-        
-        const newUser = {
-            id: users.length > 0 ? users[users.length - 1].id + 1 : 1,
-            ...req.body,
-            password: bcrypt.hashSync(req.body.password, 10),
-            avatar: req.file ? req.file.filename : 'default.png'
-        };
+    processRegister: async (req, res) => {
+        try {
+            await db.User.create({
+                first_name: req.body.first_name,
+                last_name: req.body.last_name,
+                email: req.body.email,
+                password: bcrypt.hashSync(req.body.password, 10),
+                profile_image: req.file ? req.file.filename : 'default.png'
+            });
 
-        users.push(newUser);
-        fs.writeFileSync(usersFilePath, JSON.stringify(users, null, 2));
-
-        res.redirect('/users/login');
+            return res.redirect('/users/login');
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send('Error al registrar usuario');
+        }
     },
 
     login: (req, res) => {
-        res.render('users/login');
+        return res.render('users/login');
     },
 
-    processLogin: (req, res) => {
-        const users = getUsers();
-        const userToLogin = users.find(user => user.email === req.body.email);
+    processLogin: async (req, res) => {
+        try {
+            const userToLogin = await db.User.findOne({
+                where: { email: req.body.email }
+            });
 
-        if (userToLogin) {
-            const isPasswordValid = bcrypt.compareSync(req.body.password, userToLogin.password);
+            if (userToLogin) {
+                const isPasswordValid = bcrypt.compareSync(req.body.password, userToLogin.password);
 
-            if (isPasswordValid) {
-                req.session.userLogged = userToLogin;
-                // Cambiamos el res.send por el redirect al perfil
-                return res.redirect('/users/profile');
+                if (isPasswordValid) {
+                    req.session.userLogged = userToLogin;
+                    return res.redirect('/users/profile');
+                }
+
+                return res.render('users/login', {
+                    errors: {
+                        password: {
+                            msg: 'Las credenciales son inválidas'
+                        }
+                    }
+                });
             }
 
             return res.render('users/login', {
                 errors: {
-                    password: {
-                        msg: 'Las credenciales son inválidas'
+                    email: {
+                        msg: 'No se encuentra este email en nuestra base de datos'
                     }
                 }
             });
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send('Error en el proceso de login');
         }
-
-        return res.render('users/login', {
-            errors: {
-                email: {
-                    msg: 'No se encuentra este email en nuestra base de datos'
-                }
-            }
-        });
     },
 
-    // Nuevas funciones
     profile: (req, res) => {
-        res.render('users/profile', {
+        return res.render('users/profile', {
             user: req.session.userLogged
         });
     },
