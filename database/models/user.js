760a04d36926363ddcb724f765da905555a0cbb0
GF-admin1 <gbulaciaferro02@gmail.com>
2026-08-16 15:44:49 -0300

    Update user.js

--- database/models/user.js
+++ database/models/user.js
@@ -1,1 +1,47 @@
+module.exports = (sequelize, dataTypes) => {
+    let alias = 'User';
+    let cols = {
+        id: {
+            type: dataTypes.INTEGER(10).UNSIGNED,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        first_name: {
+            type: dataTypes.STRING(100),
+            allowNull: false
+        },
+        last_name: {
+            type: dataTypes.STRING(100),
+            allowNull: false
+        },
+        email: {
+            type: dataTypes.STRING(150),
+            allowNull: false,
+            unique: true
+        },
+        password: {
+            type: dataTypes.STRING(255),
+            allowNull: false
+        },
+        profile_image: {
+            type: dataTypes.STRING(255)
+        },
+        category_id: {
+            type: dataTypes.INTEGER(10).UNSIGNED
+        }
+    };
+    let config = {
+        tableName: 'users',
+        timestamps: false
+    };
+    const User = sequelize.define(alias, cols, config);
 
+    User.associate = function(models) {
+        User.belongsTo(models.Category, {
+            as: 'category',
+            foreignKey: 'category_id'
+        });
+    }
+
+    return User;
+};
